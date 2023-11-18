8d16beab78418d13dfd6478200ff89931b0da8b5
Kenneth Doerflein <ktd500@verizon.net>
2023-11-17 21:07:31 -0500

    Add new folder button

--- FileSystemExplorer.java
+++ FileSystemExplorer.java
@@ -8,6 +8,7 @@ public class FileSystemExplorer extends JPanel implements ActionListener {
   Color white = new Color(220, 220, 220);
   Color gray = new Color(54, 59, 65);
   static JButton newFileButton;
+  static JButton newFolderButton;
   static JFrame frame;
   static ArrayList<FSObject> FSObjects;
   final static int toolBarHeight = 70;
@@ -18,12 +19,20 @@ public class FileSystemExplorer extends JPanel implements ActionListener {
     setMaximumSize(size);
     setMinimumSize(size);
     setLayout(null);
+    Rectangle buttonBounds = new Rectangle(300, 250, 100, 30);
     newFileButton = new JButton("New File");
     newFileButton.addActionListener(this);
-    newFileButton.setBounds(300, 250, 100, 30);
+    newFileButton.setBounds(buttonBounds);
     newFileButton.setVisible(true);
     add(newFileButton);
     newFileButton.setLocation(12, 20);
+
+    newFolderButton = new JButton("New Folder");
+    newFolderButton.addActionListener(this);
+    newFolderButton.setBounds(buttonBounds);
+    newFolderButton.setVisible(true);
+    add(newFolderButton);
+    newFolderButton.setLocation(newFileButton.getWidth() + 20, 20);
   }
 
   @Override
@@ -49,9 +58,6 @@ public class FileSystemExplorer extends JPanel implements ActionListener {
 
       if (tempObject.getType().equals("file")) imagePath = "./fileIcon.png";
       else if (tempObject.getType().equals("folder")) imagePath = "./folderIcon.png";
-
-//      if (i % 2 == 0) imagePath = "./fileIcon.png";
-//      else imagePath = "./folderIcon.png";
 
       ImageIcon temp = new ImageIcon(imagePath);
       int rowSpacingX = 40 + i * 2 * temp.getIconWidth();
@@ -86,6 +92,14 @@ public class FileSystemExplorer extends JPanel implements ActionListener {
       if (fileName != null && fileName.isEmpty()) fileName = "temp";
       if (fileName != null) {
         temp = new FSObject(fileName, "file");
+        FSObjects.add(temp);
+      }
+    } else if (e.getActionCommand().equals("New Folder")) {
+      String folderName = JOptionPane.showInputDialog(frame, "Enter a folder name:", null);
+      FSObject temp;
+      if (folderName != null && folderName.isEmpty()) folderName = "temp";
+      if (folderName != null) {
+        temp = new FSObject(folderName, "folder");
         FSObjects.add(temp);
       }
     }
